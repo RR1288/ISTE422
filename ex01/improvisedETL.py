c5a7391c7f04c014eee69a9ffd5e88b1108b4cb7
RR1288 <rer1288@rit.edu>
2024-09-06 14:36:35 -0400

    Test improvisedETL.py

--- ex01/improvisedETL.py
+++ ex01/improvisedETL.py
@@ -4,10 +4,21 @@ import json
 import csv
 import time
 import os
+from tkinter import messagebox
+import tkinter as tk
 
+
+def showMessage(type, message):
+    # Initialize tk
+    root = tk.Tk()
+    # Hide the main tk window
+    root.withdraw() 
+    # Show message
+    messagebox.showinfo(type, message)
+    # Destroy tk window
+    root.destroy()
 
 def main():
-    
     try:
         # Open file data.json
         with open('./data.json') as file:
@@ -36,17 +47,22 @@ def main():
         # Close output file
         csv_file.close()
 
-        # Print file path
-        print("File created in path: ", os.getcwd())
+        # Show file path
+        showMessage("Info", f"""File created in path: {os.getcwd()}""")
+        
+
+        print(f"""File created in path: {os.getcwd()}""")
 
     except FileNotFoundError:
-        print("Sorry, could not find data.json")
-        print("Make sure the file is located in your current working directory (CWDs)...")
-        print("CWD: ", os.getcwd())
+        # Show file path
+        showMessage("Error",f"""Sorry, could not find data.json in path: {os.getcwd()}""")
+        print(f"""Sorry, could not find data.json. Make sure the file is located in your current working directory (CWDs)""")
+        print(f"""CWD: {os.getcwd()}""")
 
     except Exception as e:
-        print("Sorry, an error ocurred")
-        print("Error: ", e)
+        showMessage("Error", f"""Sorry, an error ocurred: {e}""")
+        print(f"""Sorry, an error ocurred: {e}""")
+        
 
 
 if __name__ == "__main__":
